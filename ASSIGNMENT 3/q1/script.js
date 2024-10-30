ba384d91f66eb92069533a5453cf3ea190b4a496
Rohit_R_Magar <magarrohit1953@gmail.com>
2024-10-30 15:16:05 +0530

    assignment 4

---
+++ ASSIGNMENT 3/q1/script.js
@@ -0,0 +1,8 @@
+var myBtn = document.getElementById("btn");
+//font fontsize and color of text should change
+function modify() {
+    var m = document.getElementById("para");
+    m.style.color = "green";
+    m.style.fontSize = "large";
+    m.style.fontFamily = "Cascadia Code";
+}
